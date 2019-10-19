5884ca677c1781210f4134416d0fa4116ea98b1a
root <root@localhost.localdomain>
2019-10-19 20:33:10 +0300

    update

---
+++ vulnerabilities/linux/readme_dirty.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+print "This exploit uses the pokemin exploit of the dirtycow vulnerabillity."
+
+print "MAKE SURE TO CHECK THAT THE HOST IS RUNNING ON KERNEL 2.6.22 < 3.9 "
+
+print "To use it, follow the steps as shown:"
+
+print "1. Make sure to transfer the exploit to the victim machine (/tmp or var/tmp) "
+
+print "2. compile it using << gcc -pthread dirty.c -o dirty -lcrypt >>"
---
+++ vulnerabilities/windows/ms11-080/readme.md
@@ -0,0 +1,12 @@
+MS11-080 "afd.sys"
+---------------------------------------
+Supported Versions: xp/2003
+
+https://www.exploit-db.com/exploits/18176
+
+Run it on the client machine.
+
+python pyinstaller.py --onefile ms11-080.py (on windows!)
+
+** I already compiled the exploit and uploaded it to my private repo on "https://www.github.com/NetanelShoshan/OSCP" 
+
